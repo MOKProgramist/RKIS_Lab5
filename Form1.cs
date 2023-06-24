8e91bad46547d685881557e98b4199d7c7305faa
quick6response <64421792+quick6response@users.noreply.github.com>
2023-06-24 23:39:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RKIS_Lab5.Models;
+
+
+namespace RKIS_Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2AddData form2 = new Form2AddData();
+            form2.Show();
+            Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ModelEF modelEF = new ModelEF();
+            studentDataGridView.DataSource = modelEF.Student.ToList();
+        }
+
+        private void studentDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2AddData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RKIS_Lab5.Models;
+
+
+namespace RKIS_Lab5
+{
+    public partial class Form2AddData : Form
+    {
+        public Form2AddData()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2AddData_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
